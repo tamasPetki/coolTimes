ebf598c140674b6fa3dd059f3a8bfc1b4fac957f
tamasPetki <tonertop@gmail.com>
2018-10-16 14:54:37 +0200

    seperated components and retriever service prepared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-
-
+import { MainNewsComponent } from './main-news/main-news.component';
+import { SecondaryNewsComponent } from './secondary-news/secondary-news.component';
+import { RecentlyAddedNewsComponent } from './recently-added-news/recently-added-news.component';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNewsComponent,
+    SecondaryNewsComponent,
+    RecentlyAddedNewsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     NgbModule.forRoot(),
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
